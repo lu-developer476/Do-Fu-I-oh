6e5f6df938a469229602ed6f771e75e93428aa48
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-13 03:10:16 -0300

    Fix static asset collisions and simplify board cell labels (#19)

--- do_fu_ioh/settings.py
+++ do_fu_ioh/settings.py
@@ -75,8 +75,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / '.staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
